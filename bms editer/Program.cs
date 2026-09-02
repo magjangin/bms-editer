667c1a993ebbd424d9b0f1b41e80c9a8955ce685
magjangin <magjangin@users.noreply.github.com>
2026-09-02 19:39:16 +0900

    fix: XAML InputGesture 'Esc'를 'Escape'로 수정 및 독립실행형 배포 지원

--- bms editer/Program.cs
+++ bms editer/Program.cs
@@ -1,4 +1,5 @@
 using System;
+using System.IO;
 using Avalonia;
 
 namespace bms_editer;
@@ -9,8 +10,20 @@ internal class Program
     // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
     // yet and stuff might break.
     [STAThread]
-    public static void Main(string[] args) => BuildAvaloniaApp()
-        .StartWithClassicDesktopLifetime(args);
+    public static void Main(string[] args)
+    {
+        try
+        {
+            BuildAvaloniaApp().StartWithClassicDesktopLifetime(args);
+        }
+        catch (Exception ex)
+        {
+            var logPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "crash.log");
+            File.WriteAllText(logPath, ex.ToString());
+            Console.Error.WriteLine(ex);
+            throw;
+        }
+    }
 
     // Avalonia configuration, don't remove; also used by visual designer.
     public static AppBuilder BuildAvaloniaApp()
