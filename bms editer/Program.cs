bbfc22cb756606ce43b28a71364a2258bc6434c3
magjangin <magjangin@users.noreply.github.com>
2026-09-02 19:33:48 +0900

    refactor: 네임스페이스 및 코드 스타일 일원화 정리

--- bms editer/Program.cs
+++ bms editer/Program.cs
@@ -1,25 +1,24 @@
-﻿using Avalonia;
 using System;
+using Avalonia;
 
-namespace bms_editer
+namespace bms_editer;
+
+internal class Program
 {
-    internal class Program
-    {
-        // Initialization code. Don't use any Avalonia, third-party APIs or any
-        // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
-        // yet and stuff might break.
-        [STAThread]
-        public static void Main(string[] args) => BuildAvaloniaApp()
-            .StartWithClassicDesktopLifetime(args);
+    // Initialization code. Don't use any Avalonia, third-party APIs or any
+    // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
+    // yet and stuff might break.
+    [STAThread]
+    public static void Main(string[] args) => BuildAvaloniaApp()
+        .StartWithClassicDesktopLifetime(args);
 
-        // Avalonia configuration, don't remove; also used by visual designer.
-        public static AppBuilder BuildAvaloniaApp()
-            => AppBuilder.Configure<App>()
-                .UsePlatformDetect()
+    // Avalonia configuration, don't remove; also used by visual designer.
+    public static AppBuilder BuildAvaloniaApp()
+        => AppBuilder.Configure<App>()
+            .UsePlatformDetect()
 #if DEBUG
-                .WithDeveloperTools()
+            .WithDeveloperTools()
 #endif
-                .WithInterFont()
-                .LogToTrace();
-    }
+            .WithInterFont()
+            .LogToTrace();
 }
